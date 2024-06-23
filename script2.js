6fadb96c6634aacbccca0f90aff56680f6dc323a
Adam chetto <119348351+adam-chetto05@users.noreply.github.com>
2024-06-23 16:12:02 +0100

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,12 +1,12 @@
 let paragraph = document.querySelectorAll('p')
 console.log(paragraph)
 
-document.addEventListener('DOMContentLoaded',function () {
-
+window.addEventListener('load',function () {
+    
     let delay =1500;
     paragraph.forEach((paragraph,index)=>{
         setTimeout(()=>{
             paragraph.classList.add('visible');
-        },delay *(index + 1));
+        },delay * (index + 1));
     });
   });
